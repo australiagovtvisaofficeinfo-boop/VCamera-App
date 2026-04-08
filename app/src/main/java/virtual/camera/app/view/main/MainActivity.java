e674164fb15be067cf51538e0b274e30bb997c8d
Jasserbelloumi <your_email@example.com>
2026-04-08 03:11:56 +0100

    Complete VCamera project from all_project_files.txt

---
+++ app/src/main/java/virtual/camera/app/view/main/MainActivity.java
@@ -0,0 +1,13 @@
+package virtual.camera.app.view.main;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import virtual.camera.app.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
